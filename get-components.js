42c408cddfa9d67bb0f86f2b4406204676e850e0
Renato Inamine <renato.inamine@gmail.com>
2018-12-12 00:42:59 -0200

    teste

--- get-components.js
+++ get-components.js
@@ -13,51 +13,54 @@ var pageList = JSON.parse(fs.readFileSync('sites.json', 'utf8')).pages;
 // }
 
 async function run(url) {
+	console.log('>> run:', url);
 	const browser = await puppeteer.launch({
-		headless: true,
+		headless: false,
 		defaultViewport: {
 			width: 1280,
 			height: 900
 		}
 	});
 	const page = await browser.newPage();
-	await page.goto(url);
+	
 	var resultObject = {
 		url: url,
 		bodyClass: '',
 		components: []
 	};
-
+	var components = [];
+	await page.goto(url);
 	await page.evaluate(() => {
 		resultObject.bodyClass = document.body.classList.value;
-		var components = Array.from(document.querySelectorAll('[data-role]'));
-
-		// remove data-role="null"
-		components = components.filter((node) => {
-			return node.dataset.role !== 'none' && node.querySelector('[data-componentname]');
-		});
-
-		components = components.map((node) => {
-			var rObj = {};
-			node = node.querySelector('[data-componentname]');
-			Array.from(node.attributes).forEach((attr) => {
-				rObj[attr.name.replace('data-', '').replace('component-', '')] = attr.value;
-			});
-			return rObj;
-		});
-		resultObject.components = components;
+		// components =  Array.from(document.querySelectorAll('[data-role]'));
 	});
 
+	// remove data-role="null"
+	// components = components.filter((node) => {
+	// 	return node.dataset.role !== 'none' && node.querySelector('[data-componentname]');
+	// });
+
+	// components = components.map((node) => {
+	// 	var rObj = {};
+	// 	node = node.querySelector('[data-componentname]');
+	// 	Array.from(node.attributes).forEach((attr) => {
+	// 		rObj[attr.name.replace('data-', '').replace('component-', '')] = attr.value;
+	// 	});
+	// 	return rObj;
+	// });
+	// resultObject.components = components;
+
 	await browser.close();
-	return resultObject;
+	// return resultObject;
 };
 
 (async (pages) => {
+	var allPages = [];
 	for (const page of pages) {
 		allPages.push(await run(page));
 	}
 	console.log(JSON.stringify(allPages));
-})(list);
+})(pageList);
 
 // list.map(async (url)=> {
 //   await run(url);
