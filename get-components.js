b4855b07cd67e329b32916b0cd914bbcf3c4f085
Renato Inamine <renato.inamine@gmail.com>
2018-12-12 12:19:32 -0200

    report knorr

--- get-components.js
+++ get-components.js
@@ -3,55 +3,44 @@ const util = require('util');
 const fs = require('fs');
 var pageList = JSON.parse(fs.readFileSync('sites.json', 'utf8')).pages;
 
-// obj.pages.push('www.uol.com.br');
-// console.log(obj);
-// var json = JSON.stringify(obj);
-// fs.writeFile('sites.json', json, 'utf8', end);
-
-// function end() {
-//   process.exit();
-// }
-
 async function run(url) {
 	console.log('>> run:', url);
 	const browser = await puppeteer.launch({
-		headless: false,
+		headless: true,
 		defaultViewport: {
 			width: 1280,
 			height: 900
 		}
 	});
 	const page = await browser.newPage();
-	
-	var resultObject = {
-		url: url,
-		bodyClass: '',
-		components: []
-	};
-	var components = [];
 	await page.goto(url);
-	await page.evaluate(() => {
-		resultObject.bodyClass = document.body.classList.value;
-		// components =  Array.from(document.querySelectorAll('[data-role]'));
+	var resultObject = await page.evaluate(() => {
+		var list;
+
+		try {
+			list = Array.from(document.querySelectorAll('[data-role]')).filter((node) => {
+				return node.dataset.role !== 'none' && node.querySelector('[data-componentname]');
+			}).map((node) => {
+				var rObj = {};
+				node = node.querySelector('[data-componentname]');
+				Array.from(node.attributes).forEach((attr) => {
+					rObj[attr.name.replace('data-', '').replace('component-', '')] = attr.value;
+				});
+				return rObj;
+			});
+		} catch(e) {
+			console.log('cannot get list >>> ', e);
+		}
+		return {
+			url: '',
+			bodyClass: document.body.classList.value,
+			componentList: list
+		}
 	});
 
-	// remove data-role="null"
-	// components = components.filter((node) => {
-	// 	return node.dataset.role !== 'none' && node.querySelector('[data-componentname]');
-	// });
-
-	// components = components.map((node) => {
-	// 	var rObj = {};
-	// 	node = node.querySelector('[data-componentname]');
-	// 	Array.from(node.attributes).forEach((attr) => {
-	// 		rObj[attr.name.replace('data-', '').replace('component-', '')] = attr.value;
-	// 	});
-	// 	return rObj;
-	// });
-	// resultObject.components = components;
-
 	await browser.close();
-	// return resultObject;
+	resultObject.url = url;
+	return resultObject;
 };
 
 (async (pages) => {
@@ -59,9 +48,12 @@ async function run(url) {
 	for (const page of pages) {
 		allPages.push(await run(page));
 	}
-	console.log(JSON.stringify(allPages));
+	const json = JSON.stringify(allPages);
+	try {
+		fs.writeFile('report-components.json', json, 'utf8', () => {
+			  process.exit();
+			});
+	} catch(e) {
+		console.log("couldn't write file:", e);
+	}
 })(pageList);
-
-// list.map(async (url)=> {
-//   await run(url);
-// });
